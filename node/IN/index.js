64cbf73e5d3b668a457d7245ac4814bcc031d464
Filippo Grassano <com.filippo@outlook.com>
2024-02-05 21:34:08 -0300

    1:43:24

--- node/IN/index.js
+++ node/IN/index.js
@@ -1,14 +1,2 @@
 #!/usr/bin/env node
-import _ from "lodash";
-const note = process.argv[2];
-let code = _.random(1, 99);
-const newNote = {
-  content: note ? note : "No note provided.",
-  id: code + "." + Date.now(),
-};
-
-console.log(newNote);
-
-(function () {
-  console.log("IIFE");
-})();
+import "./src/command.js";
