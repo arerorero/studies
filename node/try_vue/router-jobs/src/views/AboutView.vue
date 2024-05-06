3601fb0cf0095f9473ba5f87923d281b437bd226
Filippo Grassano <com.filippo@outlook.com>
2024-05-06 15:54:56 -0300

    Jobs project for routes

---
+++ node/try_vue/router-jobs/src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam qui sunt
+      quam mollitia, sapiente nam, minima quo aliquid ullam dolorum natus eum
+      ea. Porro architecto dolor consequuntur facere accusantium blanditiis?
+    </p>
+  </div>
+</template>
---
+++ node/try_vue/router-jobs/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>this is home</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil reiciendis
+      cupiditate nam laboriosam, doloremque commodi deleniti dolorum quibusdam
+      voluptatem cumque asperiores! Accusantium rem quidem, dolorum deserunt
+      enim reiciendis veritatis ducimus?
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
